5a85b81b2fc5f1d82caf2c5059f0584a092eb970
EduardoMG9906 <eduardo.montoyag@udea.edu.co>
2025-02-20 07:29:58 -0500

    .

--- facturas_epm.py
+++ facturas_epm.py
@@ -62,32 +62,5 @@ if sql_file:
 
 df
 
-# #data = pd.read_csv("Tarifas_epm_limpio.csv")
 
-# st.set_page_config(
-#   page_title= "Proyecto",
-#   layout="wide"
-# )
-
-# st.title(" 📄 Facturas EPM")
-# st.sidebar.title("Opciones de Navegacion")
-
-# # 3. Implementación de la Barra de Navegación
-# menu = st.sidebar.radio(
-#     "Selecciona una opción:",
-#     ["Inicio", "Datos", "Visualización", "Configuración"]
-# )
-
-# # 4. Mostrar los Datos
-# if menu == "Datos":
-#     st.subheader("📂 Datos Generados")
-#     st.dataframe(data, use_container_width=True)
-  
-# # 5. Visualización de Datos
-# if menu == "Visualización":
-#     st.subheader("📊 Visualización de Datos")
-#     plt.figure(figsize=(10, 6))  # Crea una nueva figura antes del plot
-#     ax = sns.boxplot(x="Año", y="Compartido", data=data)  # Crea el plot en 'ax'
-#     fig = ax.get_figure()  # Obtiene la figura de 'ax'
-#     st.pyplot(fig)  # Muestra la figura en Streamlit
   
